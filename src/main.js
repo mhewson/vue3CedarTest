1dbf8b6b0ae29ecc7a30b3d394d6fe2a6e2f6eec
mhewson <mhewson@rei.com>
2022-02-04 13:08:10 -0800

    moving app styles to root lib styles. nesting was causing a vite load order error

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 
 import '@rei/cedar/dist/cdr-fonts.css';
 import '@rei/cedar/dist/cedar-compiled.css';
-import '@rei/cdr-tokens/dist/scss/cdr-tokens.scss';
+import './lib.scss';
 
 createApp(App).mount('#app')
